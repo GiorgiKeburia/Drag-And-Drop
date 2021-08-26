5d5ba2523a77d06640105b507373dd0850206172
giorgikeburia <giorgikeburia1@gmail.com>
2021-08-26 20:20:19 +0400

    up

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, printResult, resultPrase) {
+    // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
+    //     throw new Error("Incorrect  input!")
+    // }
+    var result = n1 + n2;
+    if (printResult) {
+        console.log(resultPrase + result);
+    }
+    else {
+        return result;
+    }
 }
 var num1 = 5;
 var num2 = 2.8;
-var result = add(num1, num2);
-console.log(result);
+var printResult = true;
+var resultPrase = "The result is : ";
+add(num1, num2, printResult, resultPrase);
--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, printResult: boolean, resultPrase: string) {
     // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
     //     throw new Error("Incorrect  input!")
     // }
-    return n1 + n2
+    const result = n1 + n2
+    if (printResult) {
+        console.log(resultPrase + result)
+    } else {
+        return result
+    }
 }
 const num1 = 5;
 const num2 = 2.8;
+const printResult = true;
+const resultPrase = "The result is : "
 
-const result = add(num1, num2)
-
-console.log(result)
+add(num1, num2, printResult, resultPrase)
