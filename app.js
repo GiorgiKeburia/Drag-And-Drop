42faebbb7181b4ab31cf5dd80e43b48409797046
giorgikeburia <giorgikeburia1@gmail.com>
2021-08-31 16:24:40 +0400

    test

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-function add(n1, n2, printResult, resultPrase) {
-    // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
-    //     throw new Error("Incorrect  input!")
-    // }
-    var result = n1 + n2;
-    if (printResult) {
-        console.log(resultPrase + result);
-    }
-    else {
-        return result;
-    }
+// const person: {
+//     name: string;
+//     age: number;
+// }
+var person = {
+    name: "Maxim",
+    age: 30,
+    myArr: ['Giorgi', 'Keburia']
+};
+var myArr;
+for (var _i = 0, _a = person.myArr; _i < _a.length; _i++) {
+    var value = _a[_i];
+    console.log(value);
 }
-var num1 = 5;
-var num2 = 2.8;
-var printResult = true;
-var resultPrase = "The result is : ";
-add(num1, num2, printResult, resultPrase);
--- app.ts
+++ app.ts
@@ -1,17 +1,15 @@
-function add(n1: number, n2: number, printResult: boolean, resultPrase: string) {
-    // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
-    //     throw new Error("Incorrect  input!")
-    // }
-    const result = n1 + n2
-    if (printResult) {
-        console.log(resultPrase + result)
-    } else {
-        return result
-    }
+// const person: {
+//     name: string;
+//     age: number;
+// }
+const person = {
+    name:"Maxim",
+    age: 30,
+    myArr:['Giorgi', 'Keburia']
 }
-const num1 = 5;
-const num2 = 2.8;
-const printResult = true;
-const resultPrase = "The result is : "
 
-add(num1, num2, printResult, resultPrase)
+let myArr : string[];
+
+for(const value of person.myArr){
+    console.log(value)
+}
