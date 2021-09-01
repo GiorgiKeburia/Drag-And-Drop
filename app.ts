e2320d152fb2d6dd3aeb207c46fea21259268beb
giorgikeburia <giorgikeburia1@gmail.com>
2021-09-01 17:04:35 +0400

    test

--- app.ts
+++ app.ts
@@ -1,27 +1,15 @@
-type Combinable = number | string;
-type ConversionDescription = 'as-number' | "as-string";
+let userInput: unknown;
+let userName: string;
 
-function combine(input1: Combinable, input2: Combinable, resultConvertion: ConversionDescription) {
-    let result;
+userInput = 5;
+userInput = "Max";
 
-    if (typeof input1 === "number" && typeof input2 === "number" || resultConvertion === "as-number") {
-        result = +input1 + +input2
-    } else {
-        result = input1.toString() + input2.toString()
-    }
-    return result
-    // if (resultConvertion === "as-number") {
-    //     return +result;
-    // }else{
-    //     return result.toString()
-    // }
+if (typeof userInput === "string") {
+    userName = userInput;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges)
+function generateError(message: string,code: number): never{
+    throw { message: message, errorCode: code}
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges)
-
-const combinedNames = combine("Giorgi", "Keburia", "as-string")
-console.log(combinedNames)
+generateError("An error occurred",300)
