6704a2af41ddf9f3bec586a99a272aebd836406f
giorgikeburia <giorgikeburia1@gmail.com>
2021-09-02 17:31:11 +0400

    class

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,47 @@
 "use strict";
 class Department {
-    constructor(n) {
-        this.name = n;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        // private readonly id: string;
+        // private name: string;
+        this.employees = [];
+    }
+    describe() {
+        console.log(`Department: ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(`Employees ${this.id}: ${this.employees}`);
     }
 }
-const account = new Department("Giorgi");
-console.log(account);
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, "ITDepartment");
+        this.admins = admins;
+    }
+}
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, "Accounting");
+        this.reports = reports;
+    }
+    addReports(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(`Reports:  ${this.reports}`);
+    }
+}
+const it = new ITDepartment("ITD", ["Giorgi,Max"]);
+it.describe();
+it.addEmployee("Giorgi");
+it.printEmployeeInformation();
+console.log(it);
+const accounting = new AccountingDepartment("ADI", []);
+accounting.addReports("something went wrong...");
+accounting.printReports();
+// const accountCopy = { emploees: ['Giorgi'],neame: "Devops", describe: account.describe };
+// accountCopy.describe()
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,57 @@
 class Department {
-  name: string;
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, private name: string) {}
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(`Employees ${this.id}: ${this.employees}`);
   }
 }
 
-const account = new Department("Giorgi");
-console.log(account);
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id,"ITDepartment");
+    this.admins = admins; 
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]){
+    super(id, "Accounting")
+  }
+
+  addReports(text:string){
+    this.reports.push(text)
+  }
+
+  printReports(){
+    console.log(`Reports:  ${this.reports}`);
+  }
+}
+
+const it = new ITDepartment("ITD",["Giorgi,Max"]);
+it.describe();
+it.addEmployee("Giorgi");
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment("ADI",[])
+
+accounting.addReports("something went wrong...")
+accounting.printReports()
+
+// const accountCopy = { emploees: ['Giorgi'],neame: "Devops", describe: account.describe };
+
+// accountCopy.describe()
